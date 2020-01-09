e353f17339249ab0b4f4129dce91d361210e2153
Ruan <ruancmeira@hotmail.com>
2020-01-09 00:07:41 -0300

    teste

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const path = require("path");
+const express = require("express");
 const app = express();
-
-// Serve only the static files form the directory
-app.use(express.static(__dirname + '/src'));
-
+app.use(express.static(__dirname + 'dist/angular-albumsearch'));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/src/index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'index.html'))
 });
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

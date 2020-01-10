12cd3d9835bb1d26520d786893e8f81866c9db88
Ruan <ruancmeira@hotmail.com>
2020-01-09 21:49:21 -0300

    Deploy Netlify

--- server.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const express = require("express");
-const app = express();
-app.use(express.static(__dirname + 'dist/angular-albumsearch'));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'))
-});
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
